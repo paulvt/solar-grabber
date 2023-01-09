093d062dd40c1a686284994b46ab797c76c10e06
Paul van Tilburg <paul@luon.net>
2023-01-09 21:40:04 +0100

    Enable more lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 #![doc = include_str!("../README.md")]
 #![warn(
     clippy::all,
+    missing_copy_implementations,
     missing_debug_implementations,
     rust_2018_idioms,
-    rustdoc::broken_intra_doc_links
+    rustdoc::broken_intra_doc_links,
+    trivial_numeric_casts,
+    renamed_and_removed_lints,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
 )]
 #![deny(missing_docs)]
 
