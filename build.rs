d3689f3b78594bef45194fa4b45e5ff6472772d2
Paul van Tilburg <paul@luon.net>
2026-03-14 10:26:59 +0100

    Bump the dependency on vergen-git2 to 9.1.0; update code
    Also drop the dependency on `vergen-git2`, it is only used while
    building.

--- build.rs
+++ build.rs
@@ -3,12 +3,9 @@ use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Generate the `cargo:` instructions to fill the appropriate environment variables.
-    let build = &BuildBuilder::all_build()?;
-    let git2 = &Git2Builder::all_git()?;
-
     Emitter::default()
-        .add_instructions(build)?
-        .add_instructions(git2)?
+        .add_instructions(&BuildBuilder::all_build()?)?
+        .add_instructions(&Git2Builder::all_git()?)?
         .emit()?;
 
     Ok(())
