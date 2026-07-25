8f002cfac902e360a556009b612de0ee465b8350
Paul van Tilburg <paul@luon.net>
2026-07-25 20:51:55 +0200

    Bump build-dependency on vergen-git2, drop dependency; update code

--- build.rs
+++ build.rs
@@ -1,11 +1,14 @@
 use std::error::Error;
-use vergen_git2::{BuildBuilder, Emitter, Git2Builder};
+use vergen_git2::{Build, Emitter, Git2};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let build = Build::builder().build_timestamp(true).build();
+    let git = Git2::builder().commit_timestamp(true).sha(true).build();
+
     // Generate the `cargo:` instructions to fill the appropriate environment variables.
     Emitter::default()
-        .add_instructions(&BuildBuilder::all_build()?)?
-        .add_instructions(&Git2Builder::all_git()?)?
+        .add_instructions(&build)?
+        .add_instructions(&git)?
         .emit()?;
 
     Ok(())
